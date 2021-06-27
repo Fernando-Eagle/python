fd632a2a2f9d45e6809e298952f2b0fad04c64f4
Fernando-Eagle <fernandodesousa777@gmail.com>
2021-06-27 00:36:51 -0300

    Add files via upload

--- operacoes.py
+++ operacoes.py
@@ -11,15 +11,25 @@ print('Este é um pequeno programa em Python')
 print('Estamos executando em Python3')
 print('-----------------------------------------')
 nome = input("Qual o seu nome?")
-x=5
-print(nome,"havia uma variável que era 'x' e valia:",x)
-num = int(input("Digite um número: "))
-y = num
-print(nome,'o número digitado é:',  y)
+sobrenome = input("Qual o seu sobrenome?")
+idade = input("Qual a sua idade?")
+numtest= input("Digite um número para teste:")
+import os
+if os.name =='nt':
+    os.system("cls")
+else:
+     os.system("clear")
+print('-----------------------------------------')
+print('Vamos aos Resultados das Entradas')
+print('-----------------------------------------')
+print("Nome:",nome,sobrenome,"- Idade:",idade)
+print("O número de teste digitado era:",numtest)
+num = input("Digite um número: ")
+print(nome,'o número digitado é:',num)
 print ("Como seria se tabalhássemos dois valores?")
-num1 = int(input('Entre com o 1º valor: '))
-num2 = int(input('Entre com o 2º valor: '))
-soma = num1 + num2
+num1 = input('Entre com o 1º valor: ')
+num2 = input('Entre com o 2º valor: ')
+soma = int(num1) + int(num2)
 print ("a soma é",soma)
 multi = int(num1) * int(num2)
 print ("a multiplicação é",multi)
